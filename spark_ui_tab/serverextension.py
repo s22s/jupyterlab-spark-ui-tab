30ba21ab82e364a9045749d2961622e14735e62a
Stanislav Kokaisl <stanislav.kokaisl@tiki-institut.com>
2019-07-26 10:12:38 +0200

    TIKIDSP-733 Code refactoring

--- spark_ui_tab/serverextension.py
+++ spark_ui_tab/serverextension.py
@@ -7,42 +7,58 @@ to the endpoint notebook_base_url/sparkmonitor
 TODO Create unique endpoints for different kernels or spark applications.
 """
 
-from notebook.base.handlers import IPythonHandler
-import tornado.web
-from tornado import httpclient
-import re
 import os
-import logging
-from bs4 import BeautifulSoup
+import re
 
-proxy_root = "/sparkuitab"
+from bs4 import BeautifulSoup
+from notebook.base.handlers import IPythonHandler
+from notebook.utils import url_path_join
+from tornado import httpclient
+from tornado import web
+
+
+try:
+    import lxml
+except ImportError:
+    BEAUTIFULSOUP_BUILDER = "html.parser"
+else:
+    BEAUTIFULSOUP_BUILDER = "lxml"
+# a regular expression to match paths against the Spark on EMR proxy paths
+PROXY_PATH_RE = re.compile(r"\/proxy\/application_\d+_\d+\/(.*)")
+# a tuple of tuples with tag names and their attribute to automatically fix
+PROXY_ATTRIBUTES = (
+    (("a", "link"), "href"),
+    (("img", "script"), "src"),
+)
+
+PROXY_ROOT = "/sparkuitab"
 
 
 class SparkMonitorHandler(IPythonHandler):
     """A custom tornado request handler to proxy Spark Web UI requests."""
 
+    @web.authenticated
     async def get(self):
         """Handles get requests to the Spark UI
 
         Fetches the Spark Web UI from the configured ports
         """
-
         http = httpclient.AsyncHTTPClient()
         baseurl = os.environ.get("SPARKMONITOR_UI_HOST", "127.0.0.1")
         port = os.environ.get("SPARKMONITOR_UI_PORT", "4040")
         url = "http://" + baseurl + ":" + port
-        request_path = self.request.uri[(
-                                                self.request.uri.index(proxy_root) + len(proxy_root) + 1):]
+        request_path = self.request.uri[(self.request.uri.index(PROXY_ROOT) + len(PROXY_ROOT) + 1):]
         self.replace_path = self.request.uri[:self.request.uri.index(
-            proxy_root) + len(proxy_root)]
-        # print("SPARKMONITOR_SERVER: Request_path " + request_path + " \n Replace_path:" + self.replace_path)
+            PROXY_ROOT) + len(PROXY_ROOT)]
+
         backendurl = url_path_join(url, request_path)
         self.debug_url = url
         self.backendurl = backendurl
-        logger.info("GET: \n Request uri:%s \n Port: %s \n Host: %s \n request_path: %s ", self.request.uri,
+        self.log.info("GET: \n Request uri:%s \n Port: %s \n Host: %s \n request_path: %s ", self.request.uri,
                     os.environ.get(
                         "SPARKMONITOR_UI_PORT", "4040"), os.environ.get("SPARKMONITOR_UI_HOST", "127.0.0.1"),
                     request_path)
+
         try:
             x = await http.fetch(backendurl)
             self.handle_response(x)
@@ -60,13 +76,13 @@ class SparkMonitorHandler(IPythonHandler):
                 self.finish()
             print("SPARKMONITOR_SERVER: Spark UI not running")
         except FileNotFoundError:
-            logger.info("default html file was not found")
+            self.log.info("default html file was not found")
 
     def handle_response(self, response):
         try:
             content_type = response.headers["Content-Type"]
             if "text/html" in content_type:
-                content = replace(response.body, self.replace_path)
+                content = self.replace(response.body, self.replace_path)
             elif "javascript" in content_type:
                 body = "location.origin +'" + self.replace_path + "' "
                 content = response.body.replace(b"location.origin", body.encode())
@@ -77,8 +93,27 @@ class SparkMonitorHandler(IPythonHandler):
             self.write(content)
             self.finish()
         except Exception as e:
-            logger.error(str(e))
+            self.log.error(str(e))
             raise e
+
+    def replace(self, content, root_url):
+        """Replace all the links with our prefixed handler links,
+
+         e.g.:
+        /proxy/application_1467283586194_0015/static/styles.css" or
+        /static/styles.css
+        with
+        /spark/static/styles.css
+        """
+        soup = BeautifulSoup(content, BEAUTIFULSOUP_BUILDER)
+        for tags, attribute in PROXY_ATTRIBUTES:
+            for tag in soup.find_all(tags, **{attribute: True}):
+                value = tag[attribute]
+                match = PROXY_PATH_RE.match(value)
+                if match is not None:
+                    value = match.groups()[0]
+                tag[attribute] = url_path_join(root_url, value)
+        return str(soup)
 
 
 def load_jupyter_server_extension(nb_server_app):
@@ -88,79 +123,10 @@ def load_jupyter_server_extension(nb_server_app):
     Args:
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
-    print("SPARKMONITOR_SERVER: Loading Server Extension")
-    # Configuring logging for the extension
-    # This is necessary because in some versions of jupyter, print statements are not output to console.
-
-    global logger
-    logger = logging.getLogger("sparkmonitorserver")
-    logger.setLevel(logging.DEBUG)
-    logger.propagate = False
-    # For debugging this module - Writes logs to a file
-    #TODO: find a more k8s way to do so
-    fh = logging.FileHandler("sparkmonitor_serverextension.log", mode="w")
-    fh.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(levelname)s:  %(asctime)s - %(name)s - %(process)d - %(processName)s - \
-        %(thread)d - %(threadName)s\n %(message)s \n")
-    fh.setFormatter(formatter)
-
     web_app = nb_server_app.web_app
-    host_pattern = ".*$"
-    route_pattern = url_path_join(
-        web_app.settings["base_url"], proxy_root + ".*")
-    web_app.add_handlers(host_pattern, [(route_pattern, SparkMonitorHandler)])
+    base_url = web_app.settings["base_url"]
+    handlers = [(PROXY_ROOT + ".*", SparkMonitorHandler)]
+    handlers = [(url_path_join(base_url, x[0]), x[1]) for x in handlers]
+    web_app.add_handlers(".*$", handlers)
 
 
-try:
-    import lxml
-except ImportError:
-    BEAUTIFULSOUP_BUILDER = "html.parser"
-else:
-    BEAUTIFULSOUP_BUILDER = "lxml"
-# a regular expression to match paths against the Spark on EMR proxy paths
-PROXY_PATH_RE = re.compile(r"\/proxy\/application_\d+_\d+\/(.*)")
-# a tuple of tuples with tag names and their attribute to automatically fix
-PROXY_ATTRIBUTES = (
-    (("a", "link"), "href"),
-    (("img", "script"), "src"),
-)
-
-
-def replace(content, root_url):
-    """Replace all the links with our prefixed handler links,
-
-     e.g.:
-    /proxy/application_1467283586194_0015/static/styles.css" or
-    /static/styles.css
-    with
-    /spark/static/styles.css
-    """
-    soup = BeautifulSoup(content, BEAUTIFULSOUP_BUILDER)
-    for tags, attribute in PROXY_ATTRIBUTES:
-        for tag in soup.find_all(tags, **{attribute: True}):
-            value = tag[attribute]
-            match = PROXY_PATH_RE.match(value)
-            if match is not None:
-                value = match.groups()[0]
-            tag[attribute] = url_path_join(root_url, value)
-    return str(soup)
-
-
-def url_path_join(*pieces):
-    """Join components of url into a relative url
-
-    Use to prevent double slash when joining subpath. This will leave the
-    initial and final / in place
-    """
-    initial = pieces[0].startswith("/")
-    final = pieces[-1].endswith("/")
-    stripped = [s.strip("/") for s in pieces]
-    result = "/".join(s for s in stripped if s)
-    if initial:
-        result = "/" + result
-    if final:
-        result = result + "/"
-    if result == "//":
-        result = "/"
-    return result
