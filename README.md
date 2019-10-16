8fb33ebf6a6f251ec375794eb8a2657dcfc61aa9
Simeon H.K. Fitch <fitch@astraea.io>
2019-10-16 11:37:56 -0400

    Astraea-specific tweaks.

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ pip install jupyterlab-spark-ui-tab
 you need to install both the lab extension as well as the server extension:
 
 ```bash
-jupyter labextension install @liorbaber/jupyterlab-spark-ui-tab
-jupyter serverextension enable --py spark-ui-tab
+jupyter labextension install @s22s/jupyterlab-spark-ui-tab
+jupyter serverextension enable --py spark_ui_tab
 ```
 
 
@@ -25,6 +25,7 @@ jupyter serverextension enable --py spark-ui-tab
 For a development install (requires npm version 4 or later), do the following in the repository directory:
 
 ```bash
+python setup.py build bdist_wheel
 npm install
 npm run build
 jupyter labextension link .
--- setup.py
+++ setup.py
@@ -3,25 +3,23 @@
 from setuptools import setup, find_packages
 from subprocess import check_output
 
-
-
 setup(name='jupyterlab-spark-ui-tab',
-      version= "0.0.1",
-      description='Spark UI extension for jupyterlab',
+      version="0.0.2",
+      description='Spark UI extension for JupyterLab',
       author='Lior Baber',
       author_email='liorbaber@gmail.com',
       include_package_data=True,
       packages=find_packages(),
       license="apache-2.0",
-      url = 'https://github.com/Liorba/jupyterlab-spark-ui-tab',
-      keywords = ['jupyter', 'extension', 'spark'],
-      classifiers = [
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: Apache Software License',
-      'Programming Language :: Python :: 3',
-          ],
+      url='https://github.com/s22s/jupyterlab-spark-ui-tab',
+      keywords=['jupyter', 'extension', 'spark'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3',
+      ],
       zip_safe=False,
       install_requires=[
-          'bs4' ,
+          'bs4',
       ]
       )
